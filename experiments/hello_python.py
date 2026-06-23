e9d0aa9e391420a56d305e1d8426eaa83ab17348
amna hassan <amnahassan@amnas-MacBook-Air-3.local>
2026-06-23 12:10:31 +0500

    Added PMW Day 1 research and experiments

---
+++ experiments/hello_python.py
@@ -0,0 +1,12 @@
+print("PMW Day 1 Experiment")
+
+methods = [
+    "COLMAP",
+    "NeRF",
+    "Gaussian Splatting"
+]
+
+print("\nMethods Researched:")
+
+for method in methods:
+    print("-", method)
